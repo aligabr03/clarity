fe5c33fdadb707d87cf6e4729ab5b642344de233
Ali Gabr <aligabr03@gmail.com>
2024-12-25 12:31:57 +0200

    chore: Setup pages file structure

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { routes } from './app.routes';
+import { HomePageComponent } from './app/pages/home-page/home-page.component';
+import { provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(HomePageComponent, {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(), provideRouter(routes)],
+})
