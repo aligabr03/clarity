6b6d3b2256f6cc856ddfe08d1d829b03d8681cf3
Ali Gabr <aligabr03@gmail.com>
2024-12-25 13:59:07 +0200

    chore: PrimeNG setup

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { Card } from 'primeng/card';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ButtonModule, Card],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
--- client/src/main.ts
+++ client/src/main.ts
@@ -4,7 +4,20 @@ import { HomePageComponent } from './app/pages/home-page/home-page.component';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import theme from './theme';
 
 bootstrapApplication(HomePageComponent, {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(), provideRouter(routes)],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: theme,
+      },
+      ripple: true
+    })],
 })
