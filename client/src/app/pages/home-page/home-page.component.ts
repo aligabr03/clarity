a1ca58a8527bb5753203318950d0a0bb6987c319
Ali Gabr <aligabr03@gmail.com>
2024-12-27 09:02:32 +0200

    feat(navbar): Basic navbar init

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { BasicButtonComponent } from '../../components/basic-button/basic-button.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
-    selector: 'app-home-page',
-    imports: [BasicButtonComponent],
-    templateUrl: './home-page.component.html',
-    styleUrl: './home-page.component.css'
+  selector: 'app-home-page',
+  imports: [BasicButtonComponent, NavbarComponent],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
