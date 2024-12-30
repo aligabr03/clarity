590d183f1ece2d228cc86c3650f33595c99ad9d9
Ali Gabr <aligabr03@gmail.com>
2024-12-30 19:39:26 +0200

    feat(signup): Basic sign up components

--- client/src/app/pages/signup-page/signup-page.component.ts
+++ client/src/app/pages/signup-page/signup-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { SignupFormComponent } from '../../components/signup-form/signup-form.component';
 
 @Component({
   selector: 'app-signup-page',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, SignupFormComponent],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css',
 })
