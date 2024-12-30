d0e7b3ddc36608f6f49592e7f6016c8e87b6b0b4
Ali Gabr <aligabr03@gmail.com>
2024-12-30 19:23:14 +0200

    feat(home): Improved alignment

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { BasicButtonComponent } from '../../components/basic-button/basic-button.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [BasicButtonComponent, NavbarComponent],
+  imports: [NavbarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
