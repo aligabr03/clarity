a4cea0960156f51309bf46bd2cf8859f4ba17575
Ali Gabr <aligabr03@gmail.com>
2025-04-05 23:54:01 -0400

    feat(navbar): User menu tweaks

--- client/src/app/components/user-menu/user-menu.component.ts
+++ client/src/app/components/user-menu/user-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthStateService } from '../../services/auth-state.service';
 import { inject } from '@angular/core';
+import { LinkComponent } from '../link/link.component';
 
 @Component({
   selector: 'app-user-menu',
-  imports: [],
+  imports: [LinkComponent],
   templateUrl: './user-menu.component.html',
   styleUrl: './user-menu.component.css'
 })
